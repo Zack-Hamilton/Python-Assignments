baf757c2696503931b67bb00a997041afc70d29e
Zack-Hamilton <ahmadhaaroon30@gmail.com>
2021-06-07 21:54:16 +0500

    Update to program after my teacher showed us how he did it. I think i understand how variable work in modules a bit better now

--- A Level 1/A_Level_1.py
+++ A Level 1/A_Level_1.py
@@ -1,25 +1,49 @@
 import random;
-num = [0] * 10;
+MAX = 10;
 
-def fill():
-    row = 0;
-    while (row < 10):
-        num[row] = random.randint(10,99);
-        row = row + 1;
+def menu():
+    print("1) Fill");
+    print("2) Print");
+    print("3) Linear Search");
+    print("7) Quit");
 
-def printing():
-    row = 0;
-    while (row < 10):
-        print(num[row]);
-        row = row + 1;    
+def fill(data):
+    for i in range (0, MAX):
+        data[i] = random.randint(10,99);
 
+def printing(data):
+    for i in range (0, MAX):
+        print(data[i]);
+
+def linear_search(data,search_item):
+    found = False;
+    i = 0;
+    while (i < MAX) and not(found):
+        if (data[i] == search_item):
+            found= True;
+        else:
+            i = i + 1;
+    return found;
+
+arr = [0] * MAX;
+item_to_search = 0;
 choice = 0;
 
 while (choice != 7):
+    menu();
     choice = int(input("Enter your choice:"));
     print("");
 
     if (choice == 1):
-        fill();
+        fill(arr);
     elif (choice == 2):
-        printing();
+        printing(arr);
+    elif (choice == 3):
+        item_to_search = int(input("Enter what you are looking for:"));
+        print("");
+        if (linear_search(arr, item_to_search) ==  True):
+            print("Item found");
+            print("");
+        else:
+            print("Item not found");
+            print("");
